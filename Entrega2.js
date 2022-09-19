9b9f6c5f300dd0d4698315156dba6fca5df1a9d5
danimorera <danimoreramunoz@gmail.com>
2022-09-20 00:25:39 +0200

    "Entrega 2 ready, review before submitting"

--- Entrega2.js
+++ Entrega2.js
@@ -2,7 +2,7 @@
 /////Ex1////////////////////////////////////////////////////////
 /*
 Mostra per la consola el resultat d'una arrow function
- autoinvocable que sumi dos nombres.
+autoinvocable que sumi dos nombres.
 */
 const suma = (() => 10 + 2 );
 console.log(suma());
@@ -15,6 +15,7 @@ retorni un objecte amb un atribut que tingui com a
 valor el paràmetre rebut.
 */
 const createObject = (value) => ({value});
+
 
 /////Ex2////////////////////////////////////////////////////////
 /*
@@ -38,8 +39,23 @@ alumne.dirNom();
 
 //Nivell 3
 /////Ex1////////////////////////////////////////////////////////
-//////////////
 /*
 Escriu una function creadora d'objectes que faci instàncies 
 d'una classe abstracta. Invoca-la amb diferents definicions.
 */
+
+class Abstracta {
+    constructor (name) {
+        if (this.constructor === Abstracta){
+            throw new Error("This is an abstract class and cannot be instantiated");
+        };
+        this.name = name;
+    };
+};
+
+class Concreta extends Abstracta {};
+
+const dog = new Concreta("Milu");
+const cat = new Concreta("Garfield");
+const duck = new Concreta("Donald");
+const idea = new Abstracta("Liberalisme");
